05da64f64f4d247e8fc377c3d20f50aac61e53d6
nicdraffin <nic.draffin@gmail.com>
2023-12-06 19:44:59 -0700

    Create appt_manager.py
    Functions are defined

---
+++ appt_manager.py
@@ -0,0 +1,18 @@
+def main():
+    pass
+
+def print_menu():
+    pass
+
+def create_weekly_calendar():
+    pass
+
+def load_scheduled_appointments():
+    pass
+
+def find_appointment_by_time():
+    pass
+
+def show_appointment_by_name():
+    pass
+
